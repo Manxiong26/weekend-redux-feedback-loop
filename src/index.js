854caf6c44bcba0c088cf545919a55fc7afaf4e7
Maneena Xiong <xiong.maneena@gmail.com>
2021-05-06 22:56:50 -0500

    intial setup and created a store and reducer for feedBack

--- src/index.js
+++ src/index.js
@@ -4,5 +4,34 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {applyMiddleware, createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
+
+const feedBack = (state = [], action ) => {
+    console.log('Hello from the feed back Reducer');
+    if(action.type === 'ADD_FEEDBACK'){
+        return action.payload
+    }
+    return state;
+}
+
+const storeInstance = createStore(
+    //this makes it so you can call multiple reducers
+    combineReducers({
+    //reducers go inside our store - these are specific to our app
+    feedBack
+    }),
+    //Also add our middleware for the logger
+    applyMiddleware(
+      logger
+    )
+  );
+
+ReactDOM.render(
+<React.StrictMode>
+<Provider store={storeInstance}>
+    <App />
+  </Provider>
+</React.StrictMode>, document.getElementById('root'));
 registerServiceWorker();
